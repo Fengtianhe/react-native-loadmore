43d4fd695d1104933f544f9f284058806cb51a45
fengtianhe <545704061@qq.com>
2018-12-24 16:05:22 +0800

    add props

--- README.md
+++ README.md
@@ -17,9 +17,12 @@ or
 |key|type|required|default|desc|
 |---- |---- |---- |---- |---- |
 |renderList|React Element|true||loadmore中显示的列表|
-|loadMoreType|String|false|'click'|下拉加载的形式['click','scroll']|
+|loadMoreType|String||'click'|下拉加载的形式\['click','scroll']|
 |renderList|React Element|true||loadmore中显示的列表|
-|onClickLoadMore|function|false||点击加载更多回调 when loadMoreType = 'click'|
-|onLoadMore|function|false||点击加载更多回调 when loadMoreType = 'scroll'|
-|onRefresh|function|false||下拉刷新调用|
-|isLastPage|bool|false|false|是否是最后一页，用于是否显示加载更多|
+|onClickLoadMore|function|loadMoreType == 'click'||点击加载更多回调 <br> when loadMoreType = 'click'|
+|onLoadMore|function|loadMoreType == 'scroll'||点击加载更多回调 <br> when loadMoreType = 'scroll'|
+|onRefresh|function|true||下拉刷新调用|
+|isLastPage|bool||false|是否是最后一页，用于是否显示加载更多|
+|isLoading|bool|true||是否是获取数据中|
+|indicatorText|bool||正在加载 ...|加载中的文本|
+|loadMoreBtnText|bool||点击加载更多 ...|点击加载的文本|
--- index.js
+++ index.js
@@ -1,98 +1,95 @@
 import React from 'react'
 import {RefreshControl, ScrollView, View} from "react-native";
 import LoadingMore from "./lib/LoadingMore";
-
-const PropTypes = require('prop-types');
-const createReactClass = require('create-react-class');
+import PropTypes from 'prop-types'
 
 
-const LoadMore = createReactClass({
-	propTypes: {
-		renderList: PropTypes.any.isRequired,
-		onClickLoadMore: PropTypes.func,
-		onRefresh: PropTypes.func.isRequired,
-		isLastPage: PropTypes.bool.isRequired,
-		loadMoreType: PropTypes.oneOf(['click', 'scroll']),
-		onLoadMore: PropTypes.func
-		// isRefreshing: React.PropTypes.bool,
-		// isNeedRefresh: React.PropTypes.bool,
-	},
-	getDefaultProps() {
-		return {
-			isRefreshing: false,
-			isNeedRefresh: true,
-			isLastPage: false,
-			loadMoreType: 'click'
-		}
-	},
-
-	/**
-	 * scrollview滑动的时候
-	 * @private
-	 */
-	_onScroll(event) {
-		if (this.props.isLastPage || this.props.loadMoreType !== 'scroll') {
-			return;
-		}
-		let y = event.nativeEvent.contentOffset.y;
-		let height = event.nativeEvent.layoutMeasurement.height;
-		let contentHeight = event.nativeEvent.contentSize.height;
-		// console.log('offsetY-->' + y);
-		// console.log('height-->' + height);
-		// console.log('contentHeight-->' + contentHeight);
-    if (y + height >= contentHeight && this.props.onLoadMore) {
-      // this.props.onLoadMore && this.props.onLoadMore()
-      return (
-        <LoadingMore
-          onLoading={() => {
-            this.props.onLoadMore()
-          }}
-        />
-      );
-		}
-	},
-
-	/**
-	 * 显示上啦加载view
-	 * @private
-	 */
-	_renderLoadMore() {
-		if (this.props.isLastPage) {
-			return;
-		}
-		return (
-			<LoadingMore
-				onLoading={() => {
-					this.props.onClickLoadMore && this.props.onClickLoadMore()
-				}}
-			/>
-		);
-	},
-
-	render() {
-		return (
-			<ScrollView
-				refreshControl={
-					<RefreshControl
-						refreshing={this.props.isRefreshing}
-						onRefresh={() => {
-							this.props.onRefresh && this.props.onRefresh()
-						}}
-						tintColor="#000"
-						title="下拉刷新"
-						titleColor="#000"
-						colors={['#ff0000', '#00ff00', '#0000ff']}
-						progressBackgroundColor="#ffffff"
-					/>
-				}
-				onScroll={nativeEvent => this._onScroll(nativeEvent)}
-				scrollEventThrottle={80}
-			>
-				{this.props.renderList}
-				{/*尾部上拉加载更多view*/}
-				{this.props.loadMoreType == 'click' ? this._renderLoadMore() : (<View/>)}
-			</ScrollView>
-		)
-	}
-})
-module.exports = LoadMore
+export default class LoadMore extends React.Component {
+  static propTypes = {
+    renderList: PropTypes.any.isRequired,
+    onClickLoadMore: PropTypes.func,
+    onRefresh: PropTypes.func.isRequired,
+    isLastPage: PropTypes.bool.isRequired,
+    loadMoreType: PropTypes.oneOf(['click', 'scroll']),
+    onLoadMore: PropTypes.func,
+    isLoading: PropTypes.bool.isRequired,
+    indicatorText: PropTypes.string,
+    loadMoreBtnText: PropTypes.string,
+  }
+  
+  static propTypes = {
+    isRefreshing: false,
+    isNeedRefresh: true,
+    isLastPage: false,
+    loadMoreType: 'click',
+    indicatorText: '正在加载 ...',
+    loadMoreBtnText: '点击加载更多 ...'
+  }
+  
+  /**
+   * scrollview滑动的时候
+   * @private
+   */
+  _onScroll (event) {
+    if (this.props.isLastPage || this.props.loadMoreType !== 'scroll') {
+      return;
+    }
+    let y = event.nativeEvent.contentOffset.y;
+    let height = event.nativeEvent.layoutMeasurement.height;
+    let contentHeight = event.nativeEvent.contentSize.height;
+    // console.log('offsetY-->' + y);
+    // console.log('height-->' + height);
+    // console.log('contentHeight-->' + contentHeight);
+    
+    if (y + height >= contentHeight && !this.props.isLoading) {
+      this.props.onLoadMore && this.props.onLoadMore()
+    }
+  }
+  
+  /**
+   * 显示上啦加载view
+   * @private
+   */
+  _renderLoadMore () {
+    if (this.props.isLastPage) {
+      return;
+    }
+    return (
+      <LoadingMore
+        isLoading={this.props.isLoading}
+        onLoading={() => {
+          this.props.onClickLoadMore && this.props.onClickLoadMore()
+        }}
+        showClickLoadmoreBtn={this.props.loadMoreType === 'click'}
+        indicatorText={this.props.indicatorText}
+        loadMoreBtnText={this.props.loadMoreBtnText}
+      />
+    );
+  }
+  
+  render () {
+    return (
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={this.props.isRefreshing}
+            onRefresh={() => {
+              this.props.onRefresh && this.props.onRefresh()
+            }}
+            tintColor="#000"
+            title="下拉刷新"
+            titleColor="#000"
+            colors={['#ff0000', '#00ff00', '#0000ff']}
+            progressBackgroundColor="#ffffff"
+          />
+        }
+        onScroll={nativeEvent => this._onScroll(nativeEvent)}
+        scrollEventThrottle={80}
+      >
+        {this.props.renderList}
+        {/*尾部上拉加载更多view*/}
+        {this._renderLoadMore()}
+      </ScrollView>
+    )
+  }
+}
